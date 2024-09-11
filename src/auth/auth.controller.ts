fa67a39cf070e82b155ce0f873abe9f3bc6ff6c6
gen <kyliki@yandex.ru>
2024-09-12 00:46:55 +0300

    установил randomstring, js-sha256

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Response } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { ConfigService } from '@nestjs/config';
-import * as express from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -9,15 +8,8 @@ export class AuthController {
     private configService: ConfigService,
     private authService: AuthService,
   ) {}
-
-  @Get('move')
-  moveToRoute(@Response() response: express.Response) {
-    const app = this.configService.get('app');
-    response.redirect(`${app.frontend}?token=true`);
-  }
-
-  @Get('login')
-  login() {
-    return this.authService.signIn('code');
+  @Get('verification')
+  verification() {
+    return res.redirect(this.configService.get('app.frontend'));
   }
 }
