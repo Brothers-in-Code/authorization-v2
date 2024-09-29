00367464cb375f84a470de82eef11a969c845ecd
ppulin <pavel.pulin@smile2impress.com>
2024-09-29 20:03:04 +0300

    fix command

--- src/scan/scan-service/scan-service.service.ts
+++ src/scan/scan-service/scan-service.service.ts
@@ -5,25 +5,26 @@ import { Cron } from '@nestjs/schedule';
 export class ScanServiceService {
   @Cron('0 12 * * *')
   async run() {
+    console.log('go');
     // params from config
-    const groups: { id: string }[] = []; // extract groups
-    for (let i = 0; i < groups.length; i++) {
-      const groupId = groups[i].id;
-      const token = ''; // update token
-      await this.scanGroup(token, groupId, 'week');
-    }
+    // const groups: { id: string }[] = []; // extract groups
+    // for (let i = 0; i < groups.length; i++) {
+    //   const groupId = groups[i].id;
+    //   const token = ''; // update token
+    //   await this.scanGroup(token, groupId, 'week');
+    // }
   }
-  async scanGroup(token: string, groupID: string, time: string) {
-    const lastDat = new Date() - Number(time);
-    let currentDate = new Date();
-    while (currentDate > lastDat) {
-      const posts = []; // load posts from VK
-      for (let i = 0; i < posts.length; i++) {
-        console.log('Save post to db');
-      }
-
-      const lastPost = posts[posts.length - 1];
-      lastDat = lastPost.date;
-    }
-  }
+  // async scanGroup(token: string, groupID: string, time: string) {
+  //   const lastDat = new Date() - Number(time);
+  //   let currentDate = new Date();
+  //   while (currentDate > lastDat) {
+  //     const posts = []; // load posts from VK
+  //     for (let i = 0; i < posts.length; i++) {
+  //       console.log('Save post to db');
+  //     }
+  //
+  //     const lastPost = posts[posts.length - 1];
+  //     lastDat = lastPost.date;
+  //   }
+  // }
 }
