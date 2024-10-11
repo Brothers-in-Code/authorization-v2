2a6d9a73ce9ad51e5dea840b122d38c133683242
gen <kyliki@yandex.ru>
2024-10-11 23:01:30 +0300

    active link in nav

--- src/modules-render/work-space/work-space.controller.ts
+++ src/modules-render/work-space/work-space.controller.ts
@@ -6,9 +6,9 @@ export class WorkSpaceController {
   constructor(private readonly workSpaceService: WorkSpaceService) {}
 
   @Get('work-space/:id')
-  @Render('pages/work-space')
+  @Render('pages/groups')
   renderWorkSpace(@Param('id') id: string) {
-    const data = { page_title: 'Рабочее пространство', user_id: id };
+    const data = { page_title: 'Группы ВК', user_id: id };
     return { data };
   }
 
@@ -16,21 +16,26 @@ export class WorkSpaceController {
   @Render('pages/groups')
   async renderGroups(@Param('id') id: string) {
     const userGroupList = await this.workSpaceService.getGroupList(Number(id));
-    const data = { pageTitle: 'Группы ВК', userId: id, userGroupList };
+    const data = {
+      pageTitle: 'Группы ВК',
+      userId: id,
+      currentPage: 'groups',
+      userGroupList,
+    };
     return { data };
   }
 
   @Get('work-space/:id/posts')
   @Render('pages/posts')
   renderPosts(@Param('id') id: string) {
-    const data = { page_title: 'Посты', user_id: id };
+    const data = { page_title: 'Посты', user_id: id, currentPage: 'posts' };
     return { data };
   }
 
   @Get('work-space/:id/reports')
   @Render('pages/reports')
   renderReports(@Param('id') id: string) {
-    const data = { page_title: 'Отчеты', user_id: id };
+    const data = { page_title: 'Отчеты', user_id: id, currentPage: 'reports' };
 
     return { data };
   }
