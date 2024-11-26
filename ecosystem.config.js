c51512dbe59ce359e836158300e6c3f24a5d3aec
ppulin <pavel.pulin@smile2impress.com>
2024-11-26 12:40:38 +0200

    revert one server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,5 +15,18 @@ module.exports = {
         ...process.env,
       },
     },
+    {
+      name: 'app-scan',
+      exec_mode: 'cluster',
+      script: './dist/main.js',
+      instances: 1,
+      env: {
+        LAUNCH_TYPE: 'pm2',
+        PORT: 9000,
+        SCAN_ENABLED: 'true',
+        SCAN_SCHEDULE: '0 12 * * *',
+        ...process.env,
+      },
+    },
   ],
 };
