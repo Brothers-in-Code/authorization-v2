3e4d920c9734898f21a1be6e5fba27432c4567cb
gen <kyliki@yandex.ru>
2024-12-08 12:29:37 +0300

    filter implementation and test

---
+++ src/shared/filter/internal-serve-exception/internal-serve-exception.filter.ts
@@ -0,0 +1,30 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(InternalServerErrorException)
+export class InternalServeExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(InternalServeExceptionFilter.name);
+
+  catch(exception: InternalServerErrorException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest();
+
+    if (exception instanceof InternalServerErrorException) {
+      this.logger.warn(
+        `filter: InternalServeExceptionFilter; catch internal server error: ${exception.message}`,
+        exception.stack,
+      );
+      const redirectTo = request.originalUrl;
+      response.redirect(`/internal-server-error?redirectTo=${redirectTo}`);
+    } else {
+      throw exception;
+    }
+  }
+}
--- src/shared/filter/unauthorized-exception/unauthorized-exception.filter.spec.ts
+++ src/shared/filter/unauthorized-exception/unauthorized-exception.filter.spec.ts
@@ -1,6 +1,18 @@
+import {
+  ArgumentsHost,
+  InternalServerErrorException,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UnauthorizedExceptionFilter } from './unauthorized-exception.filter';
 
 describe('UnauthorizedExceptionFilter', () => {
+  let filter: UnauthorizedExceptionFilter;
+
+  beforeEach(() => {
+    filter = new UnauthorizedExceptionFilter();
+  });
+
   it('should be defined', () => {
     expect(new UnauthorizedExceptionFilter()).toBeDefined();
   });
