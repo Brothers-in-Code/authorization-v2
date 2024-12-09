17eced43f3dcf2d6062b72d0402c704114669617
gen <kyliki@yandex.ru>
2024-12-09 23:48:34 +0300

    put in one describe

--- src/modules/auth/services/auth.service.spec.ts
+++ src/modules/auth/services/auth.service.spec.ts
@@ -6,7 +6,7 @@ import { UserService } from 'src/db/services/user.service';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/db/entities/user.entity';
 
-describe('saveUser', () => {
+describe('AuthService Unit Tests', () => {
   let authService: AuthService;
   let userService: UserService;
 
@@ -42,77 +42,49 @@ describe('saveUser', () => {
     userService = module.get<UserService>(UserService);
   });
 
-  it('should update user', async () => {
-    const mockParams = {
-      user_vkid: 100,
-      access_token: 'test_access_token',
-      refresh_token: 'test_refresh_token',
-      device_id: 'test_device_id',
-      expires_date: new Date(),
-    };
-
-    jest
-      .spyOn(userService, 'findOne')
-      .mockResolvedValueOnce(mockParams as User);
-
-    const spyUpdateToken = jest
-      .spyOn(userService, 'updateToken')
-      .mockResolvedValue(mockParams as User);
-
-    const response = await authService.saveUser(
-      mockParams.user_vkid,
-      mockParams.access_token,
-      mockParams.refresh_token,
-      mockParams.device_id,
-      mockParams.expires_date,
-    );
-    expect(userService.findOne).toHaveBeenCalledWith(mockParams.user_vkid);
-    expect(spyUpdateToken).toHaveBeenCalledWith(mockParams);
-    expect(response).toEqual(mockParams);
-  });
-});
-
-describe('AuthService Unit Test', () => {
-  let service: AuthService;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        AuthService,
-        {
-          provide: UserService,
-          useValue: {
-            user_vkid: 123,
-          },
-        },
-        {
-          provide: ConfigService,
-          useValue: {},
-        },
-        {
-          provide: HttpService,
-          useValue: {},
-        },
-        {
-          provide: JwtService,
-          useValue: {},
-        },
-      ],
-    }).compile();
-
-    service = module.get<AuthService>(AuthService);
-  });
-
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
   });
 
-  it('should return expires date', async () => {
-    const expire_in = 3600;
-    const date = new Date();
-    date.setSeconds(date.getSeconds() + expire_in);
-    const result = service.calcExpiresDate(expire_in);
-    const dateResult = new Date(result).getTime();
-    expect(dateResult).toBeLessThanOrEqual(date.getTime());
+  describe('calcExpiresDate', () => {
+    it('should return expires date', async () => {
+      const expire_in = 3600;
+      const date = new Date();
+      date.setSeconds(date.getSeconds() + expire_in);
+      const result = authService.calcExpiresDate(expire_in);
+      const dateResult = new Date(result).getTime();
+      expect(dateResult).toBeLessThanOrEqual(date.getTime());
+    });
+  });
+
+  describe('saveUser', () => {
+    it('should update user', async () => {
+      const mockParams = {
+        user_vkid: 100,
+        access_token: 'test_access_token',
+        refresh_token: 'test_refresh_token',
+        device_id: 'test_device_id',
+        expires_date: new Date(),
+      };
+
+      jest
+        .spyOn(userService, 'findOne')
+        .mockResolvedValueOnce(mockParams as User);
+
+      const spyUpdateToken = jest
+        .spyOn(userService, 'updateToken')
+        .mockResolvedValue(mockParams as User);
+
+      const response = await authService.saveUser(
+        mockParams.user_vkid,
+        mockParams.access_token,
+        mockParams.refresh_token,
+        mockParams.device_id,
+        mockParams.expires_date,
+      );
+      expect(userService.findOne).toHaveBeenCalledWith(mockParams.user_vkid);
+      expect(spyUpdateToken).toHaveBeenCalledWith(mockParams);
+      expect(response).toEqual(mockParams);
+    });
   });
 });
