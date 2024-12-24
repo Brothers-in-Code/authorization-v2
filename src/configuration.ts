1dd437c415f2d5239c8897bcadb4633cff280823
gen <kyliki@yandex.ru>
2024-12-25 00:16:21 +0300

    fix configuration

--- src/configuration.ts
+++ src/configuration.ts
@@ -33,13 +33,6 @@ export function configuration() {
     db: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-    integrationDb: {
-      host: process.env.INTEGRATION_DB_HOST,
-      port: process.env.INTEGRATION_DB_PORT,
       username:
         process.env.NODE_ENV === 'production'
           ? process.env.DB_USER
